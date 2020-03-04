f0c9526f9f0414b21df6f88a024f378414a94bdc
jonathanahern <modestfool@gmail.com>
2020-03-04 12:52:45 -0500

    Add particles

--- src/index.js
+++ src/index.js
@@ -1,61 +1,22 @@
-const containerElement = document.getElementById('p5-container');
-let pitch;
-let audioContext;
+import './styles/index.scss';
+import sketch from "./scripts/sketch";
 
-const sketch = (p) => {
-  let x = 100;
-  let y = 100;
-  let mic;
-  let freq = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const containerElement = document.getElementById("p5-container");
 
-  p.setup = function () {
-    // p.createCanvas(800, 400);
-    p.userStartAudio();
-    audioContext = p.getAudioContext();
-    mic = new p5.AudioIn();
-    mic.start(listening);
-  };
-
-  function listening(){
-    pitch = ml5.pitchDetection(
-      "./crepe",
-      audioContext,
-      mic.stream,
-      modelLoaded
-    );
+  function startListening() {
+    new p5(sketch, containerElement);
   }
 
-  function gotPitch(error, frequency) {
-    if (error) {
-      console.error(error);
-    } else {
+  startListening();
 
-      var vol = mic.getLevel() * 100;
-      if (frequency && vol > 0.7) {
-        freq = frequency;
-        console.log("Vol: " + vol.toString() + " Freq: " + freq.toString());
-      }
-
-    }
-    pitch.getPitch(gotPitch);
-  }
-
-  function modelLoaded(){
-    // console.log('model loaded!');
-    pitch.getPitch(gotPitch);
-  }
+  // window.onload = function() {
+  //   Particles.init({
+  //     selector: ".background"
+  //   });
+  // };
 
 
-  p.draw = function () {
-    // p.background(0);
-    // p.fill(255);
-    // p.rect(x, y, 50, 50);
-    // var vol = mic.getLevel() * 100;
-    // if (vol > .7){
-    // // console.log(vol);
-    // }
-  };
+});
 
-};
 
-new p5(sketch, containerElement);
---
+++ src/scripts/sketch.js
@@ -0,0 +1,63 @@
+import ml5 from "ml5";
+// import "../styles/index";
+
+const sketch = (p) => {
+  // let x = 100;
+  // let y = 100;
+  let mic;
+  let freq = 0;
+  let pitch;
+  let audioContext;
+
+
+  p.setup = function () {
+    // p.createCanvas(800, 400);
+    p.userStartAudio();
+    audioContext = p.getAudioContext();
+    mic = new p5.AudioIn();
+    mic.start(listening);
+  };
+
+  function listening(){
+    pitch = ml5.pitchDetection(
+      "./crepe",
+      audioContext,
+      mic.stream,
+      modelLoaded
+    );
+  }
+
+  function gotPitch(error, frequency) {
+    if (error) {
+      console.error(error);
+    } else {
+
+      var vol = mic.getLevel() * 100;
+      if (frequency && vol > 0.7) {
+        freq = frequency;
+        console.log("Vol: " + vol.toString() + " Freq: " + freq.toString());
+      }
+
+    }
+    pitch.getPitch(gotPitch);
+  }
+
+  function modelLoaded(){
+    // console.log('model loaded!');
+    pitch.getPitch(gotPitch);
+  }
+
+
+  p.draw = function () {
+    // p.background(0);
+    // p.fill(255);
+    // p.rect(x, y, 50, 50);
+    // var vol = mic.getLevel() * 100;
+    // if (vol > .7){
+    // // console.log(vol);
+    // }
+  };
+
+};
+
+export default sketch;
